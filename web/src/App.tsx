8bc097177b96dacbe88e3f9eb04df83ed273670d
Márcio Pereira <marciop.usa@gmail.com>
2023-01-24 22:14:57 -0300

    frontend: header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from './components/Header';
 import './styles/global.css';
 
-function App() {
-  return <h1 className="text-cyan-200">Hello</h1>
+export function App() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+        <div></div>
+      </div>
+    </div>
+  )
 }
-
-export default App
