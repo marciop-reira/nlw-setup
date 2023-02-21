74821f8f95e3fa887a4237cf8659e80bc31c758e
Márcio Pereira <marciop.usa@gmail.com>
2023-02-21 13:01:48 -0300

    mobile: fix formula

--- mobile/src/utils/generate-progress-percentage.ts
+++ mobile/src/utils/generate-progress-percentage.ts
@@ -1,5 +1,5 @@
 export function generateProgressPercentage(total: number, completed: number) {
   return total > 0
-    ? Math.round(completed / total) * 100
+    ? Math.round((completed / total) * 100)
     : 0;
 }
