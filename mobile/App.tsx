d1807a095235f31838b17d0c58586a446dad7dde
Márcio Pereira <marciop.usa@gmail.com>
2023-02-21 19:53:08 -0300

    mobile: local notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,9 +6,19 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold, useFonts
 } from '@expo-google-fonts/inter';
+import * as Notifications from 'expo-notifications';
+import { useEffect } from "react";
 import { StatusBar } from 'react-native';
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  })
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,6 +27,28 @@ export default function App() {
     Inter_700Bold,
     Inter_800ExtraBold
   });
+
+  async function scheduleNotification() {
+    const trigger = new Date(Date.now());
+    trigger.setSeconds(trigger.getSeconds() + 1);
+  
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Habit",
+        body: "Você abriu o app!"
+      },
+      trigger
+    });
+  }
+
+  async function getScheduledNotifications() {
+    const notifications = await Notifications.getAllScheduledNotificationsAsync();
+    console.log(notifications);
+  }
+
+  useEffect(() => {
+    scheduleNotification();
+  }, []);
 
   if (!fontsLoaded) {
     return (
